5a852cab852fd197841f99eb9dc5c41797b15917
Yvette <nchombuayvta@gmil.com>
2020-05-28 20:36:06 +0200

    "almost complete"

--- module 3-day 8/helper.js
+++ module 3-day 8/helper.js
@@ -1,64 +1,140 @@
-const url = "https://striveschool.herokuapp.com/api/product/";window.onload =  getProducts = async () => {
-  let response = await fetch(url,{
+
+const url = "https://striveschool.herokuapp.com/api/product/";
+
+// for the index.html page
+const getProducts = async () => {
+let response = await fetch(url,{
       "method":"GET",
       "headers":{
         "Authorization": "Basic " + btoa('user30:E6tYs6PBzufRfsVP')
       }  }); 
   const data = await response.json(); 
   return data;
-};window.onload = async function getProductsDetails ()  {
-    let productsDiv = document.querySelector(".container");
-    let products = await getProducts();
-    console.log(products)   
-     if (products.length > 0) {
-      products.forEach((pdt) => {
-        productsDiv.innerHTML+= `
-        <div class= "card mb-3" style="width: 18rem;">
-       <img src="${pdt.imageUrl}" class="card-img-top" alt="...">
-      <div class="card-body">
-      <h5 class="card-title">${pdt.name}</h5>
-      <p class="card-text">${pdt.description}</p>
-      <p class="card-text">${pdt.brand}</p>
-      <p class="card-text">${pdt.price}</p>
-      
-     <a href="#" class="btn btn-primary">Details</a>
-  </div>
-</div>
-        `
-      });
-    } else {
-      productsDiv.innerHTML = "<h1>No selected products</h1>";
-    }
-  };const saveProduct = async (product) => {
+};
+
+
+  
+  // for the backoffice.html page
+  const saveProduct = async (product) => {
   let response = await fetch(url, {
     method: "POST",
     body: JSON.stringify(product),
     headers: new Headers({
       "Content-Type": "application/json",
-      "Authorization": "Basic " + btoa('user30:E6tYs6PBzufRfsVP')    }),
+      "Authorization": "Basic " + btoa('user30:E6tYs6PBzufRfsVP')  }),
   });
   return response;
 }; 
 
-handleSubmit = () => {
+const handleSubmit = () => {
     event.preventDefault();
     submitProduct();
+  //we could have written the submitProduct fxn here but bc we need it else where, we write it separately
   };  
   
-submitProduct = async () => {
+  submitProduct = async () => {
+  // create a spinner to show the webpage is processing first(not oligatory)
+    let spinner = document.querySelector(".spinner-border")
+    spinner.classList.toggle("d-none");
     let myProduct = {
-      imageUrl:document.querySelector("img").src,
+      imageUrl:document.querySelector("#imageUrl").value,
       name: document.querySelector("#name").value,
       description: document.querySelector("#description").value,
       brand: document.querySelector("#brand").value,
       price: document.querySelector("#price").value,
     };
-    console.log("MYPRODUCT", myProduct);    let response = await saveProduct(myProduct);
+    console.log("MYPRODUCT", myProduct);    
+    let response;
+
+    if (id){
+      response = await editProduct(id, myProduct)
+    }else{
+      response = await saveProduct(myProduct)
+    }
+    
     console.log("RESPONSE FROM THE SAVEPRODUCT", response);
     if (response.ok) {
       alert("Product created successfully");
+      spinner.classList.toggle("d-none");
       location.assign("index.html");
     } else {
+      spinner.classList.toggle("d-none")
       alert("product NOT created succesfully");
     }
+  };
+
+
+
+  // for the detail.html page
+
+  const getSingleProduct = async (id)=> {
+    let response = await fetch(url + id, {
+      "method":"GET",
+      "headers":{
+        "Authorization": "Basic " + btoa('user30:E6tYs6PBzufRfsVP')
+      }  });
+
+      const data = await response.json()
+      return data;
+ 
+  }
+
+
+  // Delete function
+  const deleteProduct = async (id) =>{
+  let response = await fetch(url + id,{
+    method: "DELETE",
+    "headers":{
+      "Authorization": "Basic " + btoa('user30:E6tYs6PBzufRfsVP')
+    }  }); 
+    return response;
+
+  }
+
+  const handleDelete = async () =>{
+    const response = await deleteProduct(id);
+    if(response.ok){
+      alert("Event deleted successfully");
+      window.location.assign("index.html")
+    }else{
+      alert("Event has not been deleted successfully")
+    }
+  }
+
+
+  // Edit function
+
+  const editProduct = async (id,product)=> {
+    let response = await fetch(url + id, {
+      method: "PUT",
+      body: JSON.stringify(product),
+    headers: new Headers({
+      "Content-Type": "application/json",
+      "Authorization": "Basic " + btoa('user30:E6tYs6PBzufRfsVP')    }),
+    });
+    return response;
+  };
+
+window.onload = async () =>{
+  let urlParams = new URLSearchParams(window.location.search)
+  id =urlParams.get("id");
+  if(id){
+    console.log("ID IS PRESENT, FETCHING DETAILS", id);
+    let pdt = await editProduct(id);
+    console.log("PRODUCT DETAILS", product);
+       document.querySelector("#imageUrl").value = pdt.img;
+       document.querySelector("#name").value = pdt.name;
+       document.querySelector("#description").value= pdt.description;
+       document.querySelector("#brand").value = pdt.brand
+       document.querySelector("#price").value= pdt.price
+
+  }else{
+    console.log("I WANT TO CREATE AN ELEMENT FROM SCRATCH")
+  }
+
+}
+
+  const handleEdit = () => {
+    window.location.href = "backoffice.html?id =" + id;
+
   };
